b976b2598c631a7ad53caba6b571df52657ba019
schlaBAM <brett.morrison@live.com>
2018-01-09 18:57:02 -0800

    Initial Commit

---
+++ mapperPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapperPrototype
+//
+//  Created by BAM on 2018-01-09.
+//  Copyright © 2018 BAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
